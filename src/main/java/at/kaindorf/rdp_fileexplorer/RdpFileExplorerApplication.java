acf3b5c631b67596714eb0c5f33442e3fb63b77a
Manuel Reicht <reichtmanuel01@gmail.com>
2023-04-19 08:12:00 +0200

    initial commit

---
+++ src/main/java/at/kaindorf/rdp_fileexplorer/RdpFileExplorerApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.rdp_fileexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdpFileExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdpFileExplorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/kaindorf/rdp_fileexplorer/RdpFileExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.rdp_fileexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdpFileExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
